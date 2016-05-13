42ba28ccedfe2fb2a93ef5db7fb4553cf26605fd
Haonan Liu <liuhaonan00@sina.com>
2016-05-13 18:22:47 +1000

    email verification

---
+++ src/controller/EmailVerifyServlet.java
@@ -0,0 +1,44 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class EmailVerifyServlet
+ */
+@WebServlet(name = "EmailVerifyServlet", urlPatterns = "/emailverify")
+public class EmailVerifyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EmailVerifyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("EmailVerify Servlet");
+		int userId = Integer.parseInt(request.getParameter("user_id"));
+		System.out.println(userId);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
