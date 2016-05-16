b9fb57849a46b28a97ff4be465495d73534bcec1
rcapizzi <rei_it10@hotmail.com>
2016-05-17 00:49:31 +1000

    owner page commit2

--- src/controller/MaintenanceServlet.java
+++ src/controller/MaintenanceServlet.java
@@ -20,7 +20,7 @@ public class MaintenanceServlet extends HttpServlet {
 		Connection connection = o.DBConnect();
 		String query = "SELECT room_no FROM room WHERE room_id IN (SELECT room_id FROM room_status WHERE room_status.status = 'maintenance' AND room_status.hotel_id = " + hotel_id + ")";
 		ResultSet rs = o.searchDB(connection, query);
-		ArrayList<String> maintRooms = new ArrayList<String>();
+		ArrayList<String> maintRooms = new ArrayList<>();
 		while(rs.next()){
 			String i = rs.getString(1);
 			maintRooms.add(i);
@@ -34,7 +34,7 @@ public class MaintenanceServlet extends HttpServlet {
 		Connection connection = o.DBConnect();
 		String query = "SELECT room_no FROM room WHERE room_id IN (SELECT room_id FROM room_status WHERE room_status.status = 'occupied' AND room_status.hotel_id = " + hotel_id + ")";
 		ResultSet rs = o.searchDB(connection, query);
-		ArrayList<String> occRooms = new ArrayList<String>();
+		ArrayList<String> occRooms = new ArrayList<>();
 		while(rs.next()){
 			String i = rs.getString(1);
 			occRooms.add(i);
@@ -42,21 +42,66 @@ public class MaintenanceServlet extends HttpServlet {
 		return occRooms;
 	}
 	
+	public String getHotelName(String hotel_id) throws SQLException
+	{
+		MysqlOperation o = new MysqlOperation();
+		Connection connection = o.DBConnect();
+		String query = "SELECT hotel_name FROM hotel WHERE hotel_id = " + hotel_id +"";
+		ResultSet rs = o.searchDB(connection, query);
+		String hotelName = "";
+		while(rs.next()){
+			hotelName = rs.getString(1);
+		}
+		return hotelName;
+	}
+	
+	public String getHotel_Id(String hotel_name) throws SQLException
+	{
+		MysqlOperation o = new MysqlOperation();
+		Connection connection = o.DBConnect();
+		String query = "SELECT hotel_id FROM hotel WHERE hotel_name = " +"'"+ hotel_name +"'";
+		ResultSet rs = o.searchDB(connection, query);
+		String hotelName = "";
+		while(rs.next()){
+			hotelName = rs.getString(1);
+		}
+		return hotelName;
+	}
+	
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String hotel_id = request.getParameter("hotel");
-		ArrayList<String> maintRooms = new ArrayList<String>();
-		ArrayList<String> occRooms = new ArrayList<String>();
+		String hotel_id = ""; 
+		hotel_id = request.getParameter("hotel");
+		if (hotel_id.length() > 0){
+		ArrayList<String> maintRooms = new ArrayList<>();
+		ArrayList<String> occRooms = new ArrayList<>();
+		String hotelName = "";
 		try{
 			maintRooms = maintenanceRoomNames(hotel_id);
 			occRooms = occupiedRoomNames(hotel_id);
+			hotelName = getHotelName(hotel_id);
 		}catch (Exception e){
 			e.printStackTrace();
 		}
+		
 		RequestDispatcher rd = request.getRequestDispatcher("/ownerMaintenance.jsp");
 		request.setAttribute("occupied", occRooms);
 		request.setAttribute("maintenance", maintRooms);
+		request.setAttribute("hotel_name", hotelName);
 		rd.forward(request, response);
+		return;
+		}
+		
+//		INSERT INTO room_status (hotel_id, room_id,booking_id, status, start_date, end_date, booking_price) VALUES
+//		(1,1,2,'Booked', '2016-07-28','2016-07-30','100');
+		
+		String maintHotelName = "";
+		String maintRoomName = "";
+		
+		
+		
 		
 
 }
+
 }
