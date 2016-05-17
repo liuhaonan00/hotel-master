6fa5bf6987fd8084d3a433d2b77de907a55f00b0
Haonan Liu <liuhaonan00@sina.com>
2016-05-17 20:07:06 +1000

    add booking confirmation email function

--- src/businessLogic/library/emailTest.java
+++ src/businessLogic/library/emailTest.java
@@ -3,6 +3,6 @@ package businessLogic.library;
 public class emailTest {
 	public static void main(String [] args) {
 		EmailApi e = new EmailApi();
-		e.verifyEmail(2, "liuhaonan", "liuhaonan00@gmail.com");
+		e.sendBookingConfirmation(2, "liuhaonan", "liuhaonan00@gmail.com", "1234");
 	}
 }
