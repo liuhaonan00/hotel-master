838488f7ca8b2bec603ea29dcf54ffb50bfcb6b2
antiheaven <priestma@gmail.com>
2016-05-18 13:28:23 +1000

    add a url in email

--- src/businessLogic/library/EmailApi.java
+++ src/businessLogic/library/EmailApi.java
@@ -84,6 +84,7 @@ public class EmailApi {
 					  + "Your booking ID is: "+bookingId+", "
 					  + "and your pin code is: "+pin+" <br><br>"
 					  + "You can user your bookingID and pin to check your bookings <br><br>"
+					  + "via http://localhost:8080/assignment2/reviewbooking.jsp?"+bookingId
 					  + "Best Regards";
 			
 			generateMailMessage.setContent(emailBody, "text/html");
